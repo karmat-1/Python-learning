b31e51c3efb3f71b2852a432daab2a54a749100d
Abdulkareem Abdulmateen <162720141+karmat-1@users.noreply.github.com>
2025-05-21 11:38:48 +0100

    day3

---
+++ codes/day3/exercise1.py
@@ -0,0 +1,13 @@
+# Create a Function to Calculate Factorials.
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def print_factorial(n):
+    result = factorial(n)
+    print(f"The factorial of {n} is {result}")
+    
+print_factorial(0)
---
+++ codes/day3/exercise2.py
@@ -0,0 +1,10 @@
+# Import Custom Module for Arithmetic operation
+import math_operations as mo
+
+num1 = 10
+num2 = 5
+
+print("Addition: ", mo.add(num1, num2))
+print("Subtraction: ", mo.subtract(num1, num2))
+print("Multiplication: ", mo.multiply(num1, num2))
+print("Division: ", mo.divide(num1, num2))
